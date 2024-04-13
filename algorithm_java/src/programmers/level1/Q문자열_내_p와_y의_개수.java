8e09b710fce36de23eebb321e7d23b06fad85872
wlshooo <kswlsgh@naver.com>
2024-04-13 22:08:13 +0900

    문자열 내 p와 y의 개수

---
+++ algorithm_java/src/programmers/level1/Q문자열_내_p와_y의_개수.java
@@ -0,0 +1,23 @@
+package programmers.level1;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Q문자열_내_p와_y의_개수 {
+    class Solution {
+        boolean solution(String s) {
+            boolean answer = true;
+            int pCount = 0;
+            int yCount = 0;
+            s = s.toUpperCase();
+            for (int i = 0; i < s.length(); i++) {
+                if (s.charAt(i) == 'P') pCount++;
+                else if (s.charAt(i) == 'Y') yCount++;
+            }
+            if (pCount != yCount) answer = false;
+
+            return answer;
+        }
+    }
+}
