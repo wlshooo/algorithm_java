e08f1486035c74af96b7b3656ff57098deadde25
wlshooo <kswlsgh@naver.com>
2024-04-20 08:34:32 +0900

    문자열 정수로 바꾸기

---
+++ algorithm_java/src/programmers/level1/Q문자열을_정수로_바꾸기.java
@@ -0,0 +1,10 @@
+package programmers.level1;
+
+public class Q문자열을_정수로_바꾸기 {
+    class Solution {
+        public int solution(String s) {
+            int answer = Integer.parseInt(s);
+            return answer;
+        }
+    }
+}
