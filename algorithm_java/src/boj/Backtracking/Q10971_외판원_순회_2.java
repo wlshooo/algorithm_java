16e60a15c82407cbb2c220abb6d6f1d416ca66c9
wlshooo <kswlsgh@naver.com>
2024-05-03 15:20:06 +0900

    S2 외판원 순회2

---
+++ algorithm_java/src/boj/Backtracking/Q10971_외판원_순회_2.java
@@ -0,0 +1,56 @@
+package boj.Backtracking;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+//S2
+public class Q10971_외판원_순회_2 {
+    static int N, MIN;
+    static int[][] graph;
+    static boolean[] visit;
+
+    public static void TSP(int start, int prev, int depth, int sum) {
+        if (depth == N - 1) {
+            if (graph[prev][start] == 0)
+                return;
+            MIN = Math.min(MIN, sum + graph[prev][start]);
+            return;
+        }
+
+        for (int next = 0; next < N; next++) {
+            if (!visit[next] && graph[prev][next] != 0) {
+                visit[next] = true;
+                TSP(start, next, depth + 1, sum + graph[prev][next]);
+                visit[next] = false;
+            }
+        }
+    }
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        N = Integer.parseInt(br.readLine());
+        graph = new int[N][N];
+        MIN = Integer.MAX_VALUE;
+        visit = new boolean[N];
+
+        for (int i = 0; i < N; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            for (int j = 0; j < N; j++) {
+                graph[i][j] = Integer.parseInt(st.nextToken());
+            }
+        }
+
+        for (int i = 0; i < N; i++) {
+            visit[i] = true;
+            TSP(i, i, 0, 0);
+            visit[i] = false;
+        }
+
+        if (MIN == Integer.MAX_VALUE) {
+            System.out.println(0);
+        } else {
+            System.out.println(MIN);
+        }
+    }
+}
