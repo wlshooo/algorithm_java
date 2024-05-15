80afa0f7e73497aaa4a9c4e4f706db7e5c3a2a86
wlshooo <kswlsgh@naver.com>
2024-05-15 15:25:54 +0900

    35.24시간

---
+++ algorithm_java/src/ready/swea/D3/Q35_24시간.java
@@ -0,0 +1,22 @@
+package ready.swea.D3;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
+public class Q35_24시간 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int T = Integer.parseInt(br.readLine());
+
+        for (int tc = 1; tc <= T; tc++) {
+            int[] array = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
+            int num = array[0] + array[1];
+            if (num >= 24) {
+                num-=24;
+            }
+            System.out.println("#" +tc+" "+num);
+        }
+    }
+}
