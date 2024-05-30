bc667b843fedfaac66b3a67ffec8de0c2f3cca28
wlshooo <kswlsgh@naver.com>
2024-05-30 18:12:08 +0900

    81.문제 제목 붙이기

---
+++ algorithm_java/src/ready/swea/D3/Q81_문제_제목_붙이기.java
@@ -0,0 +1,32 @@
+package ready.swea.D3;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Q81_문제_제목_붙이기 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int T = Integer.parseInt(br.readLine());
+
+        for (int i = 1; i <= T; i++) {
+            int N = Integer.parseInt(br.readLine());
+            int [] alpha = new int[26];
+            int count =0;
+            for (int j = 0; j < N; j++) {
+                char c = br.readLine().charAt(0);
+                alpha[(int) c - 65]++;
+            }
+            System.out.print("#"+i+" ");
+            for (int i1 : alpha) {
+                if (i1 != 0) {
+                    count++;
+                } else if (i1==0){
+                    break;
+                }
+            }
+            System.out.println(count);
+        }
+    }
+}
